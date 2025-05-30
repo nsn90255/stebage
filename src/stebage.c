3d652f6107ad6cf0799d4d2a5bf0be9154a510ab
nsn90255 <nsn90255@gmail.com>
2025-05-30 12:58:10 +0200

    Read menu.json

--- src/stebage.c
+++ src/stebage.c
@@ -2,12 +2,19 @@
 #include <cjson/cJSON.h>
 
 int menu() {
-
+    // get menu file
+    FILE *fp = fopen("data/structure/menu.json", "r");
+    if (fp == NULL) {
+	printf("Cannot access menu.json\n");
+	return 1;
+    }
+    // write file into a string
+    char buffer[2048];
+    int len = fread(buffer, 1, sizeof(buffer), fp);
+    fclose(fp);
 }
 int main(int argc, char *argv[]) {
-
     // get menu
     menu();
-
     return 0;
 }
