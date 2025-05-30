89d9830158ce842672cb12422e4cc98a979e5395
nsn90255 <nsn90255@gmail.com>
2025-05-30 13:12:20 +0200

    Rename variables

--- src/stebage.c
+++ src/stebage.c
@@ -3,15 +3,15 @@
 
 int menu() {
     // get menu file
-    FILE *fp = fopen("data/structure/menu.json", "r");
-    if (fp == NULL) {
+    FILE *menufp = fopen("data/structure/menu.json", "r");
+    if (menufp == NULL) {
 	printf("Cannot access menu.json\n");
 	return 1;
     }
     // write file into a string
-    char buffer[2048];
-    int len = fread(buffer, 1, sizeof(buffer), fp);
-    fclose(fp);
+    char menuContents[2048];
+    int len = fread(menuContents, 1, sizeof(menuContents), menufp);
+    fclose(menufp);
 }
 int main(int argc, char *argv[]) {
     // get menu
