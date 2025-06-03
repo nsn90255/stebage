0a6b1efe7a04a87c687d04cf51585ed923644faa
nsn90255 <nsn90255@gmail.com>
2025-06-03 12:08:42 +0200

    Clear terminal after input

--- src/stebage.c
+++ src/stebage.c
@@ -2,6 +2,12 @@
 #include <string.h>
 #include <cjson/cJSON.h>
 
+void clearLines(int linesPrinted) {
+    for(int i = 0; i < linesPrinted; i++) {    
+	printf("\033[A");
+	printf("\033[2K");
+    }
+}
 char menu(char startInput) {
     // get menu file
     FILE *menufp = fopen("data/structure/menu.json", "r");
@@ -21,6 +27,7 @@ char menu(char startInput) {
 	return 1;
     }
     int count = cJSON_GetArraySize(menuJson);
+    int linesPrinted = 0;
     for (int i = 0; i < count; ++i) {
         cJSON *item = cJSON_GetArrayItem(menuJson, i);
         cJSON *name = cJSON_GetObjectItem(item, "Name");
@@ -29,16 +36,21 @@ char menu(char startInput) {
         if (cJSON_IsString(name) && cJSON_IsString(value) && strcmp(name->valuestring, "StartQuestion") == 0) {
             printf("%s", value->valuestring);
 	    scanf("%c", &startInput);
+	    linesPrinted++;
         } else if (cJSON_IsString(name) && cJSON_IsString(value)) {
             printf("%s\n", value->valuestring);
+	    linesPrinted++;
         } 
     }
+    // clear terminal and return the user input
+    clearLines(linesPrinted);
     return startInput;
 }
 int main(int argc, char *argv[]) {
     // get menu
     char startInput = 'n';
     startInput = menu(startInput);
+    fflush(stdin);
     if (startInput == 'Y' || startInput == 'y' || startInput == '\n') {
 	printf("starting game\n");
     } else {
