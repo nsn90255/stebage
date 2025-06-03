2ef2571bc4b9098b843a9c3ea9e818248fe811d4
nsn90255 <nsn90255@gmail.com>
2025-06-03 10:41:48 +0200

    Print menu

--- src/stebage.c
+++ src/stebage.c
@@ -10,8 +10,26 @@ int menu() {
     }
     // write file into a string
     char menuContents[2048];
-    int len = fread(menuContents, 1, sizeof(menuContents), menufp);
+    int len = fread(menuContents, 1, sizeof(menuContents) -1, menufp);
     fclose(menufp);
+    menuContents[len] = '\0';
+    // parse values
+    cJSON *menuJson = cJSON_Parse(menuContents);
+    if (menuJson == NULL) {
+	printf("Error parsing menu.json\n");
+	return 1;
+    }
+    int count = cJSON_GetArraySize(menuJson);
+    for (int i = 0; i < count; ++i) {
+        cJSON *item = cJSON_GetArrayItem(menuJson, i);
+        cJSON *name = cJSON_GetObjectItem(item, "Name");
+        cJSON *value = cJSON_GetObjectItem(item, "Value");
+
+        if (cJSON_IsString(name) && cJSON_IsString(value)) {
+            printf("%s\n", value->valuestring);
+        } 
+    }
+    return 0;
 }
 int main(int argc, char *argv[]) {
     // get menu
