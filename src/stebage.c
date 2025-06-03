debd13ca3dfdd4cb3f80413193be2a76d812068f
nsn90255 <nsn90255@gmail.com>
2025-06-03 12:31:20 +0200

    Add comments

--- src/stebage.c
+++ src/stebage.c
@@ -3,6 +3,7 @@
 #include <cjson/cJSON.h>
 
 void clearLines(int linesPrinted) {
+    // in this order, go up a line and clear it
     for(int i = 0; i < linesPrinted; i++) {    
 	printf("\033[A");
 	printf("\033[2K");
@@ -11,23 +12,31 @@ void clearLines(int linesPrinted) {
 char menu(char startInput) {
     // get menu file
     FILE *menufp = fopen("data/structure/menu.json", "r");
+    // return if there's a problem opening the file
     if (menufp == NULL) {
 	printf("Cannot access menu.json\n");
 	return 1;
     }
     // write file into a string
     char menuContents[2048];
+    // get size of parsed string
     int len = fread(menuContents, 1, sizeof(menuContents) -1, menufp);
+    // close file
     fclose(menufp);
+    // terminate string
     menuContents[len] = '\0';
     // parse values
     cJSON *menuJson = cJSON_Parse(menuContents);
+    // return if the menu is a null pointer
     if (menuJson == NULL) {
 	printf("Error parsing menu.json\n");
 	return 1;
     }
+    // get size of the array
     int count = cJSON_GetArraySize(menuJson);
+    // keep track of printed lines for clearing later
     int linesPrinted = 0;
+    // loop through every array item and print its value
     for (int i = 0; i < count; ++i) {
         cJSON *item = cJSON_GetArrayItem(menuJson, i);
         cJSON *name = cJSON_GetObjectItem(item, "Name");
@@ -35,6 +44,7 @@ char menu(char startInput) {
 	// print menu items, get input for starting question
         if (cJSON_IsString(name) && cJSON_IsString(value) && strcmp(name->valuestring, "StartQuestion") == 0) {
             printf("%s", value->valuestring);
+	    // ask user whether to start or not
 	    scanf("%c", &startInput);
 	    linesPrinted++;
         } else if (cJSON_IsString(name) && cJSON_IsString(value)) {
@@ -50,7 +60,7 @@ int main(int argc, char *argv[]) {
     // get menu
     char startInput = 'n';
     startInput = menu(startInput);
-    fflush(stdin);
+    // start game if the user so desires
     if (startInput == 'Y' || startInput == 'y' || startInput == '\n') {
 	printf("starting game\n");
     } else {
